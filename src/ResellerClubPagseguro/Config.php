910051014e6728b07dd1472bce84c22b907540c8
Glauber Portella <glauberportella@gmail.com>
2013-07-20 00:08:19 -0300

    Add fund handler

--- src/ResellerClubPagseguro/Config.php
+++ src/ResellerClubPagseguro/Config.php
@@ -4,8 +4,8 @@ namespace ResellerClubPagseguro;
 class Config
 {
 	// Your ResellerClub KEY for Payment Gateway
-	const RESELLERCLUB_KEY = 'nttYdk8X9PVhWU18QVSZZQFXYeiR1bn0';
-	const PAGSEGURO_RETURN_URL = 'http://resellerclub.local/resellerclub/postpayment.php';
-	const PAGSEGURO_EMAIL = 'glauberportella@gmail.com';
+	const RESELLERCLUB_KEY = 'AhBNDrHBeIcHqcZHe3usTBvvnrfXKNKC';
+	const PAGSEGURO_RETURN_URL = 'http://pagamento.desenvolve4web.com/postpayment.php';
+	const PAGSEGURO_EMAIL = 'host@desenvolve4web.com';
 	const PAGSEGURO_TOKEN = '8DAEC9E606FC409084D42A125278B756';
 }
