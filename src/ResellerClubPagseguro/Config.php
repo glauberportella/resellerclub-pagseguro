1eed85d21eb962fc3bfd9b520c3363b915691a2c
Glauber Portella <glauberportella@gmail.com>
2014-06-16 14:40:32 -0300

    Update Config.php

--- src/ResellerClubPagseguro/Config.php
+++ src/ResellerClubPagseguro/Config.php
@@ -5,7 +5,7 @@ class Config
 {
 	// Your ResellerClub KEY for Payment Gateway
 	const RESELLERCLUB_KEY = 'AhBNDrHBeIcHqcZHe3usTBvvnrfXKNKC';
-	const PAGSEGURO_RETURN_URL = 'http://pagamento.desenvolve4web.com/postpayment.php';
-	const PAGSEGURO_EMAIL = 'host@desenvolve4web.com';
+	const PAGSEGURO_RETURN_URL = 'http://site.pagamento.com/postpayment.php';
+	const PAGSEGURO_EMAIL = 'email@pagamento.com';
 	const PAGSEGURO_TOKEN = '8DAEC9E606FC409084D42A125278B756';
 }
